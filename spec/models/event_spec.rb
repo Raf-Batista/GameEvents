0a82c42503e48b7c3bb67d5d32e09b32576c21e5
Rafael Batista <RafaelB24@yahoo.com>
2019-05-04 22:18:03 -0400

    Fix errors in models/event_spec and models/user_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:description)}
-  it {should belongs_to(:game)}
+  it {should belong_to(:game)}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:password)}
   it 'is not valid when password confirmation does not match' do
-    expect(User.create(:name => "test", :email => "test@example.com", :password => "test", :password_confirmation => "test123").valid?).to eq(false)
+    expect(User.create(:username => "test", :email => "test@example.com", :password => "test", :password_confirmation => "test123").valid?).to eq(false)
   end
   it do
      should validate_length_of(:password).
