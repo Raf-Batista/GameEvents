e3739f71018107a913d95f4f7d997a8e509a9fe8
Rafael Batista <RafaelB24@yahoo.com>
2019-05-07 00:17:56 -0400

    Change Sign Up to Create Account in Sign up view and users_features spec

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'User signup', type: :feature do
         fill_in 'Email', :with => 'test@email.com'
         fill_in 'Username', :with => 'test'
         fill_in 'Password', :with => 'test123'
-        click_button 'Sign Up'
+        click_button 'Create Account'
         expect(current_path).to eq('/users/1')
         expect(page).to have_content("welcome test")
     end 
