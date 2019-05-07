d44ea4cc49c154a32f5ff5417d263bce2710f580
Rafael Batista <RafaelB24@yahoo.com>
2019-05-06 21:53:05 -0400

    Add routes to links in navbar, create Sessions and Users controller, add new view to Sessions and User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/home', :to => 'static_pages#home'
+  get '/about', :to => 'static_pages#about'
+  get '/signup', :to => 'users#new'
+  get '/login', :to => 'sessions#new'
 end
