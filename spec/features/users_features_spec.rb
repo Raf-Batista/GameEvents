fafa8926b025f209b60f190b769eb2a6d1ed9a8e
Rafael Batista <RafaelB24@yahoo.com>
2019-05-07 00:20:20 -0400

    Change User signup test to fill in correct form fields

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'User signup', type: :feature do 
     it 'signs up a user' do
         visit '/signup'
-        fill_in 'Email', :with => 'test@email.com'
-        fill_in 'Username', :with => 'test'
-        fill_in 'Password', :with => 'test123'
+        fill_in 'user_email', :with => 'test@email.com'
+        fill_in 'user_username', :with => 'test'
+        fill_in 'user_password', :with => 'test123'
         click_button 'Create Account'
         expect(current_path).to eq('/users/1')
         expect(page).to have_content("welcome test")
