76a12722fa4c2c5505d2c2f378aadbb0a4ce0ebd
Rafael Batista <RafaelB24@yahoo.com>
2019-05-08 16:20:34 -0400

    Add tests for User login

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -10,11 +10,29 @@ RSpec.describe 'User signup', type: :feature do
         expect(page).to have_content("Welcome, test")
     end 
 
-    it 'on sign up, successfully adds a session hash' do
+    it 'successfully adds a session hash on signup' do
         visit '/signup'
         fill_in 'user_username', :with => 'test'
         fill_in 'user_password', :with => 'test123'
         click_button 'Create Account'
-        expect(page.get_rack_session_key('user_id')).to_not be_nil
+        expect(page.get_rack_session_key('user_id')).to_not eq(nil)
+    end 
+
+    it 'successfully logs in' do
+        User.create(:username => 'test', :password => 'test123')
+        visit '/login' 
+        fill_in 'user_username', :with => 'test'
+        fill_in 'user_password', :with => 'test123'
+        click_button 'Log In'
+        expect(page).to have_content("Welcome, test")
+    end 
+
+    it 'successfully adds session hash on login' do
+        User.create(:username => 'test', :password => 'test123')
+        visit '/login' 
+        fill_in 'user_username', :with => 'test'
+        fill_in 'user_password', :with => 'test123'
+        click_button 'Log In'
+        expect(page.get_rack_session_key('user_id')).to_not eq(nil)
     end 
 end 
