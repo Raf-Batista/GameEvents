723c8bcfdf3e2847d802be78d0f7bc2425909207
Rafael Batista <RafaelB24@yahoo.com>
2019-05-08 11:48:25 -0400

    Remove email column from User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_001611) do
+ActiveRecord::Schema.define(version: 2019_05_08_154522) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2019_05_07_001611) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
