5dedcb4194d03c5576075e2aa173cbd3e6856f12
Rafael Batista <RafaelB24@yahoo.com>
2019-05-08 14:45:18 -0400

    Remove Email validation from User model, add seed data for new User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :username, :presence => true
-  validates :email, :presence => true
   validates :password, :length => { :in => 5..20 }
   has_many :owners
   has_many :games, :through => :owners
